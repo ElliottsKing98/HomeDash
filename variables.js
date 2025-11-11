9c908d94ebfd3b2e386f639d2bd3d29ad3579b3d
ElliottsKing98 <kermpanda@gmail.com>
2025-11-11 09:26:17 -0800

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+//Leaky's Steroid Days
+let Mondaysteroid = "No Steroid";
+let Tuesdaysteroid = "No Steroid";
+let Wednesdaysteroid = "Steroid Day";
+let Thursdaysteroid = "No Steroid";
+let Fridaysteroid = "Steroid";
+document.getElementById("Mondee").innerHTML = Mondaysteroid;
+document.getElementById("Tuesdee").innerHTML = Tuesdaysteroid;
+document.getElementById("Wendsdee").innerHTML = Wednesdaysteroid;
+document.getElementById("Thursdee").innerHTML = Thursdaysteroid;
+document.getElementById("Fridee").innerHTML = Fridaysteroid;
