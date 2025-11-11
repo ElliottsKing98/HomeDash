a1684f4ba16b09e017cca6731c47dc4ec06d03cf
ElliottsKing98 <kermpanda@gmail.com>
2025-11-11 09:55:20 -0800

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,11 +1,13 @@
 //Leaky's Steroid Days
-let Mondaysteroid = "No Steroid";
-let Tuesdaysteroid = "No Steroid";
-let Wednesdaysteroid = "Steroid Day";
-let Thursdaysteroid = "No Steroid";
-let Fridaysteroid = "Steroid Day";
-document.getElementById("Mondee").innerHTML = Mondaysteroid;
-document.getElementById("Tuesdee").innerHTML = Tuesdaysteroid;
-document.getElementById("Wendsdee").innerHTML = Wednesdaysteroid;
-document.getElementById("Thursdee").innerHTML = Thursdaysteroid;
-document.getElementById("Fridee").innerHTML = Fridaysteroid;
+window.onload = function vblz() {
+    let Mondaysteroid = "No Steroid";
+    let Tuesdaysteroid = "No Steroid";
+    let Wednesdaysteroid = "Steroid Day";
+    let Thursdaysteroid = "No Steroid";
+    let Fridaysteroid = "Steroid Day";
+    document.getElementById("Mondee").innerHTML = Mondaysteroid;
+    document.getElementById("Tuesdee").innerHTML = Tuesdaysteroid;
+    document.getElementById("Wendsdee").innerHTML = Wednesdaysteroid;
+    document.getElementById("Thursdee").innerHTML = Thursdaysteroid;
+    document.getElementById("Fridee").innerHTML = Fridaysteroid;
+}
