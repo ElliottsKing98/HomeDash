b8687570cf37bb511487fe7918be1a635d1032d3
ElliottsKing98 <kermpanda@gmail.com>
2025-11-12 13:26:55 -0800

    Update variables.js

--- variables.js
+++ variables.js
@@ -12,3 +12,7 @@ document.getElementById("Rogue4").innerHTML = `Beef Stew`;
 document.getElementById("Rogue5").innerHTML = `Gnochi Skillet`;
 document.getElementById("Rogue6").innerHTML = `Pita Chicken`;
 document.getElementById("Rogue7").innerHTML = `Soup & Sanmmich`;
+//Everyone's Updates
+document.getElementById("Sammy").innerHTML = `Has training on Sunday`;
+document.getElementById("Leaky").innerHTML = `Still sick and getting steroid`;
+document.getElementById("Udon").innerHTML = `Just ate, is in shed`;
